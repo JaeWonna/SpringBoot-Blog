3c2b4e93946289009f899b0cf12d03651d537560
JaeWonna <highjae09@gmail.com>
2023-07-29 19:03:56 +0900

    블로그 생성 / 수정 / 삭제 / 목록조회 가능

--- src/main/java/me/jaewonna/springbootdeveloper/controller/BlogApiController.java
+++ src/main/java/me/jaewonna/springbootdeveloper/controller/BlogApiController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import me.jaewonna.springbootdeveloper.domain.Article;
 import me.jaewonna.springbootdeveloper.dto.AddArticleRequest;
 import me.jaewonna.springbootdeveloper.dto.ArticleResponse;
+import me.jaewonna.springbootdeveloper.dto.UpdateArticleRequest;
 import me.jaewonna.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -50,5 +51,13 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id, @RequestBody UpdateArticleRequest request){
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
